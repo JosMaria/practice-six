826a83c955448d0197d19dd3d971007911510a8d
Jose Maria <josmariaguilar@gmail.com>
2022-05-02 11:47:39 -0400

    Creation of the first table to results

--- src/main/java/org/genesiscode/practicesix/view/ExerciseOnePane.java
+++ src/main/java/org/genesiscode/practicesix/view/ExerciseOnePane.java
@@ -1,11 +1,18 @@
 package org.genesiscode.practicesix.view;
 
+import javafx.geometry.Insets;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.VBox;
+
+import java.util.List;
 
 public class ExerciseOnePane extends MyPane {
 
     private static ExerciseOnePane exerciseOnePane;
 
+    private TableView<RowResult> table;
     private ExerciseOnePane() {
         super("EJERCICIO 1");
         loadControls();
@@ -17,9 +24,33 @@ public class ExerciseOnePane extends MyPane {
     }
 
     private void loadControls() {
+        table = new TableView<>();
+        buildTableResult();
     }
 
     private void buildPane() {
-        mainPane = new VBox(10, title);
+        mainPane = new VBox(10, title, table);
+        mainPane.setPadding(new Insets(10));
     }
+
+    private void buildTableResult() {
+        TableColumn<RowResult, Integer> colOne = new TableColumn<>("Número");
+        colOne.setCellValueFactory(new PropertyValueFactory<>("numberBall"));
+        colOne.setPrefWidth(80);
+
+        TableColumn<RowResult, Integer> colTwo = new TableColumn<>("Número\nAleatorio");
+        colTwo.setCellValueFactory(new PropertyValueFactory<>("numberRandom"));
+        colTwo.setPrefWidth(80);
+
+        TableColumn<RowResult, Integer> colThree = new TableColumn<>("Color");
+        colThree.setCellValueFactory(new PropertyValueFactory<>("color"));
+        colThree.setPrefWidth(100);
+
+        table.getColumns().addAll(List.of(colOne, colTwo, colThree));
+        table.setMaxWidth(260);
+    }
+}
+
+record RowResult(int numberBall, double numberRandom, String color) {
+
 }
